bec7d91dd8d6044bbbdb4f3439d741ffca41b115
phachon <phachon@163.com>
2018-06-06 17:58:58 +0800

    fix

--- pack.sh
+++ pack.sh
@@ -21,17 +21,17 @@ rm -rf wmqx.exe
 
 echo 'Start pack linux amd64'
 GOOS=linux GOARCH=amd64 go build -ldflags "-s -w"
-tar -czvf "${RELEASE}/wmqx-linux-amd64.tar.gz" wmqx.exe config.toml message.json log/.gitignore License.md README.md README_CN.md
+tar -czvf "${RELEASE}/wmqx-linux-amd64.tar.gz" wmqx config.toml message.json log/.gitignore License.md README.md README_CN.md
 rm -rf wmqx
 
 echo 'Start pack linux 386'
 GOOS=linux GOARCH=386 go build -ldflags "-s -w"
-tar -czvf "${RELEASE}/wmqx-linux-386.tar.gz" wmqx.exe config.toml message.json log/.gitignore License.md README.md README_CN.md
+tar -czvf "${RELEASE}/wmqx-linux-386.tar.gz" wmqx config.toml message.json log/.gitignore License.md README.md README_CN.md
 rm -rf wmqx
 
 echo 'Start pack mac amd64'
 GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w"
-tar -czvf "${RELEASE}/wmqx-mac-amd64.tar.gz" wmqx.exe config.toml message.json log/.gitignore License.md README.md README_CN.md
+tar -czvf "${RELEASE}/wmqx-mac-amd64.tar.gz" wmqx config.toml message.json log/.gitignore License.md README.md README_CN.md
 rm -rf wmqx
 
 echo 'END'
